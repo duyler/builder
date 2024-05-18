9f07e436e26f3312e22f5f360f0b9ae2cfac69d1
Mikhail Ilinsky <mail@milinsky.ru>
2024-05-18 18:11:44 +1000

    Sync with action bus

--- src/Build/Subscription/Subscription.php
+++ src/Build/Subscription/Subscription.php
@@ -7,6 +7,7 @@ namespace Duyler\Framework\Build\Subscription;
 use Duyler\ActionBus\BusBuilder;
 use Duyler\ActionBus\Dto\Subscription as SubscriptionDto;
 use Duyler\ActionBus\Enum\ResultStatus;
+use UnitEnum;
 
 class Subscription
 {
@@ -17,8 +18,11 @@ class Subscription
         static::$busBuilder = $busBuilder;
     }
 
-    public static function add(string $subjectId, string $actionId, ResultStatus $status = ResultStatus::Success): void
-    {
+    public static function add(
+        string|UnitEnum $subjectId,
+        string|UnitEnum $actionId,
+        ResultStatus $status = ResultStatus::Success,
+    ): void {
         self::$busBuilder->addSubscription(
             new SubscriptionDto(
                 subjectId: $subjectId,
